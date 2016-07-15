fc6da625a7e3f418ebe4b4d2c8c13542259c5eea
Felipe Brigalante <felipe.brigalante@gmail.com>
2016-07-14 22:15:32 -0300

    last try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,16 +79,4 @@ Rails.application.configure do
 
   # Action Mailer url options
   config.action_mailer.default_url_options = { host: 'loginsample.herokuapp.com' }
-
-  config.action_mailer.delivery_method = :smtp
-  # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
 end
